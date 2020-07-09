76285df60336a0d014c52a20850b62b5fc2519ac
Andrey Larin <lestex@gmail.com>
2020-07-08 21:54:09 -0400

    changes links to use route names

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
 <div class="test">
   <header>
     <router-link to="/" class="logo">Logo</router-link>
-    <router-link to="/" class="button">Home</router-link>
-    <router-link to="/about" class="button">About</router-link>
-    <router-link to="/login" class="button">Login</router-link>
-    <router-link to="/logout" class="button">Logout</router-link>
+    <router-link :to="{ name: 'home' }" class="button">Home</router-link>
+    <router-link :to="{ name: 'about' }" class="button">About</router-link>
+    <router-link :to="{ name: 'login' }" class="button">Login</router-link>
+    <router-link :to="{ name: 'logout' }" class="button">Logout</router-link>
   </header>
 </div>
 </template>
