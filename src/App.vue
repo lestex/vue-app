9770525d511a7ecbd28ebb1cb2a77265fcc8f828
Andrey Larin <lestex@gmail.com>
2020-07-08 21:41:07 -0400

    removes styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <Header />
+      <Header />
     <router-view/>
     <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Header from './components/Header'
+import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default {
-  name: 'Home',
+  name: 'App',
   components: {
     Header,
     Footer
@@ -20,25 +19,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
