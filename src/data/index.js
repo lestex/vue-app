0bab184d12bedeaa8bf6fdf0c41e4aca1e2b076e
Andrey Larin <lestex@gmail.com>
2020-07-10 19:44:03 -0400

    chnages datasource

--- src/data/index.js
+++ src/data/index.js
@@ -1,19 +1,3 @@
-// export default lorem => {
-//   return `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-//           Ut convallis sem ac ex auctor, in consectetur lorem posuere. 
-//           Nullam pulvinar auctor odio, sed varius erat convallis nec. 
-//           Curabitur nec lectus accumsan, congue ligula sit amet, venenatis felis. 
-//           Nunc lorem mauris, posuere vel mauris et, luctus posuere velit. 
-//           In dui mi, ultricies at maximus vel, luctus et nibh. Donec vehicula nulla 
-//           vitae ornare lobortis. Nam porta varius erat vitae gravida. 
-//           Etiam tincidunt elit arcu, ac malesuada erat tristique non. 
-//           Phasellus suscipit aliquet lacus nec convallis. In sed ante sed enim 
-//           dignissim bibendum ut eget dolor. Nulla efficitur eleifend justo, at 
-//           egestas diam pellentesque id. Morbi lorem nibh, elementum bibendum ante eu, 
-//           laoreet porttitor nunc. Duis id neque volutpat, tristique massa non, interdum enim. 
-//           Curabitur in dolor lacus.`;
-// };
-
 export default {
   lorem: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
           Ut convallis sem ac ex auctor, in consectetur lorem posuere. 
