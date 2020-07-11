8cae1a4fec6de0b9a120349d7fcc81edd4db26fa
Andrey Larin <lestex@gmail.com>
2020-07-10 23:35:36 -0400

    adds login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,30 @@
 <template>
-<form>
-  <fieldset>
-    <legend>Simple form</legend>
-    <div class="row">
-      <div class="col-sm-12 col-md-6">
-        <label for="username">Username</label>
-        <input type="text" id="Username" placeholder="Username"/>
-      </div>
-      <div class="col-sm-12 col-md-6">
-        <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Password"/>
-      </div>
-    </div>
-  </fieldset>
-</form>  
+<div class="row">
+  <div class="col-sm-8 col-md-offset-2">
+    <form>
+      <fieldset>
+        <legend class="doc">Login form</legend>
+          <div class="row responsive-label">
+            <div class="col-sm-12 col-md-3">
+              <label for="sf1-text" class="doc">Username</label>
+            </div>
+            <div class="col-sm-12 col-md">
+              <input type="text" placeholder="Username" id="sf1-text" style="width:85%;" class="doc">
+            </div>
+          </div>
+          <div class="row responsive-label">
+            <div class="col-sm-12 col-md-3">
+              <label for="sf1-pwd" class="doc">Password</label>
+            </div>
+            <div class="col-sm-12 col-md">
+                <input type="password" placeholder="Password" id="sf1-pwd" style="width:85%;" class="doc">
+            </div>
+          </div>
+      </fieldset>
+      <button class="primary">Login</button>
+    </form>
+  </div>
+</div>
 </template>
 
 <script>
