00f7addb584fc2644186763810dddb647deffe90
Andrey Larin <lestex@gmail.com>
2020-07-10 20:33:34 -0400

    removes home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
 <div class="test">
   <header>
-    <router-link to="/" class="logo">Logo</router-link>
-    <router-link :to="{ name: 'home' }" class="button">Home</router-link>
+    <router-link :to="{ name: 'home' }" class="logo">Logo</router-link>
     <router-link :to="{ name: 'about' }" class="button">About</router-link>
     <router-link :to="{ name: 'login' }" class="button">Login</router-link>
     <router-link :to="{ name: 'logout' }" class="button">Logout</router-link>
