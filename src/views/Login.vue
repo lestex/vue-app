8f387a358ab85d9672f1515d35eddc1b12256481
Andrey Larin <lestex@gmail.com>
2020-07-11 23:01:54 -0400

    updates login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,36 @@
 <template>
 <div class="row">
+  <p>User: </p>
   <div class="col-sm-8 col-md-offset-2">
-    <form>
+    <form @submit.prevent="onSubmit(email, password)">
       <fieldset>
         <legend class="doc">Login</legend>
           <div class="row responsive-label">
             <div class="col-sm-12 col-md-3">
-              <label for="sf1-text" class="doc">Username</label>
+              <label class="doc">Username</label>
             </div>
             <div class="col-sm-12 col-md">
-              <input type="text" placeholder="Username" id="sf1-text" style="width:85%;" class="doc">
+              <input
+                type="text"
+                placeholder="Username"
+                class="doc"
+                style="width:85%;"
+                v-model="email"
+              />
             </div>
           </div>
           <div class="row responsive-label">
             <div class="col-sm-12 col-md-3">
-              <label for="sf1-pwd" class="doc">Password</label>
+              <label class="doc">Password</label>
             </div>
             <div class="col-sm-12 col-md">
-                <input type="password" placeholder="Password" id="sf1-pwd" style="width:85%;" class="doc">
+                <input 
+                  type="password"
+                  placeholder="Password"
+                  style="width:85%;"
+                  class="doc"
+                  v-model="password"
+                >
             </div>
           </div>
       </fieldset>
@@ -28,8 +41,22 @@
 </template>
 
 <script>
+
 export default {
-    name: 'Form'
+  name: 'Login',
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit(email, password) {
+      console.log(email, password)
+      this.$store.dispatch('userLogin', email)
+      this.$router.push({ name: 'home' })
+    }
+  }
 }
 </script>
 
