d90c0ccaa4b7145d417372f89f5e45bbe82052a3
Andrey Larin <lestex@gmail.com>
2020-07-11 23:13:27 -0400

    updates header with login logout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,26 @@
   <header>
     <router-link :to="{ name: 'home' }" class="logo">Logo</router-link>
     <router-link :to="{ name: 'about' }" class="button">About</router-link>
-    <router-link :to="{ name: 'login' }" class="button">Login</router-link>
-    <router-link :to="{ name: 'logout' }" class="button">Logout</router-link>
+    <router-link v-if="!isAuthenticated" :to="{ name: 'login' }" class="button">Login</router-link>
+    <button v-if="isAuthenticated" @click="logout">Logout</button>
   </header>
 </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  computed: {
+    ...mapGetters(['isAuthenticated'])
+  },
+  methods: {
+    ...mapActions(['userLogout']),
+    logout(){
+      this.userLogout()
+    }
+  }
 }
 </script>
 
